854e3d950cb6ebc089f36c92b4f2b16a10c92158
root <ssingh@delaplex.in>
2020-05-11 20:48:36 +0530

    change in ticl_receiving module

--- ticl_receiving/models/tel_stock_location.py
+++ ticl_receiving/models/tel_stock_location.py
@@ -46,48 +46,48 @@ class StockMove(models.Model):
     _order = 'origin desc'
 
     #override Function for move
-    def _action_confirm(self, merge=True, merge_into=False):
-        """ Confirms stock move or put it in waiting if it's linked to another move.
-        :param: merge: According to this boolean, a newly confirmed move will be merged
-        in another move of the same picking sharing its characteristics.
-        """
-        move_create_proc = self.env['stock.move']
-        move_to_confirm = self.env['stock.move']
-        move_waiting = self.env['stock.move']
+    # def _action_confirm(self, merge=True, merge_into=False):
+    #     """ Confirms stock move or put it in waiting if it's linked to another move.
+    #     :param: merge: According to this boolean, a newly confirmed move will be merged
+    #     in another move of the same picking sharing its characteristics.
+    #     """
+    #     move_create_proc = self.env['stock.move']
+    #     move_to_confirm = self.env['stock.move']
+    #     move_waiting = self.env['stock.move']
 
-        to_assign = {}
-        for move in self:
-            # if the move is preceeded, then it's waiting (if preceeding move is done, then action_assign has been called already and its state is already available)
-            if move.move_orig_ids:
-                move_waiting |= move
-            else:
-                if move.procure_method == 'make_to_order':
-                    move_create_proc |= move
-                else:
-                    move_to_confirm |= move
-            if move._should_be_assigned():
-                key = (move.group_id.id, move.location_id.id, move.location_dest_id.id)
-                if key not in to_assign:
-                    to_assign[key] = self.env['stock.move']
-                to_assign[key] |= move
+    #     to_assign = {}
+    #     for move in self:
+    #         # if the move is preceeded, then it's waiting (if preceeding move is done, then action_assign has been called already and its state is already available)
+    #         if move.move_orig_ids:
+    #             move_waiting |= move
+    #         else:
+    #             if move.procure_method == 'make_to_order':
+    #                 move_create_proc |= move
+    #             else:
+    #                 move_to_confirm |= move
+    #         if move._should_be_assigned():
+    #             key = (move.group_id.id, move.location_id.id, move.location_dest_id.id)
+    #             if key not in to_assign:
+    #                 to_assign[key] = self.env['stock.move']
+    #             to_assign[key] |= move
 
-        # create procurements for make to order moves
-        for move in move_create_proc:
-            values = move._prepare_procurement_values()
-            origin = (move.group_id and move.group_id.name or (move.origin or move.picking_id.name or "/"))
-            self.env['procurement.group'].run(move.product_id, move.product_uom_qty, move.product_uom, move.location_id, move.rule_id and move.rule_id.name or "/", origin,
-                                              values)
+    #     # create procurements for make to order moves
+    #     for move in move_create_proc:
+    #         values = move._prepare_procurement_values()
+    #         origin = (move.group_id and move.group_id.name or (move.origin or move.picking_id.name or "/"))
+    #         self.env['procurement.group'].run(move.product_id, move.product_uom_qty, move.product_uom, move.location_id, move.rule_id and move.rule_id.name or "/", origin,
+    #                                           values)
 
-        move_to_confirm.write({'state': 'confirmed'})
-        (move_waiting | move_create_proc).write({'state': 'waiting'})
+    #     move_to_confirm.write({'state': 'confirmed'})
+    #     (move_waiting | move_create_proc).write({'state': 'waiting'})
 
-        # assign picking in batch for all confirmed move that share the same details
-        for moves in to_assign.values():
-            moves._assign_picking()
-        self._push_apply()
-        if merge and self._context.get('merge'):
-            return self._merge_moves(merge_into=merge_into)
-        return self
+    #     # assign picking in batch for all confirmed move that share the same details
+    #     for moves in to_assign.values():
+    #         moves._assign_picking()
+    #     self._push_apply()
+    #     if merge and self._context.get('merge'):
+    #         return self._merge_moves(merge_into=merge_into)
+    #     return self
 
 
 #     @api.multi
