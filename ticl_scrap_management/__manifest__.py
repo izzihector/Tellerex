b0e6bd7dca41599796f19ef28ad5dc4539d36d49
root <ssingh@delaplex.in>
2020-05-12 20:37:32 +0530

    add ticl_scrap_management module

---
+++ ticl_scrap_management/__manifest__.py
@@ -0,0 +1,30 @@
+###################################################################################
+#    Created By Delaplex
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+###################################################################################
+
+{
+    "name": "TICL Scrap Management", 
+    "summary": """TICL Scrap Management""",
+    "version": '12.0',   
+    "category": 'TICL Scrap Management',   
+    "license": "AGPL-3",
+    "website": "http://www.delaplex.in",
+    "author": "Delaplex",
+    "contributors": ["Delaplex Team"],
+    "depends": ['web', 'base', 'mail', 'product', 'sale_management','sale','sale_stock','stock','purchase','account','ticl_management','ticl_receiving','ticl_shipment','ticl_sale'],
+    "data": [ 
+        'security/ir.model.access.csv',
+        'views/stock_scrap_views.xml',
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+    'images': ['static/description/icon.png'],
+    "application": True,
+    "installable": True,
+}
---
+++ ticl_scrap_management/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    delaplex Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Delaplex.
+#    Author: Delaplex
+###################################################################################
+
+from . import ticl_scrap
+from . import stock_move
