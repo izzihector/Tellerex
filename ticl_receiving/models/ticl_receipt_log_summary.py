1e5fbad8d9d1a028178302e2864aaf9f5ad6365e
root <ssingh@delaplex.in>
2020-06-27 23:53:56 +0530

    chnage in COD image1

--- ticl_receiving/models/ticl_receipt_log_summary.py
+++ ticl_receiving/models/ticl_receipt_log_summary.py
@@ -1476,10 +1476,10 @@ class ticl_receipt_log_summary_line(models.Model):
                             table = 'class_ir_attachmentsatm_rel'
                             field_id = 'attachment_ids'
                         if sub_folder_name == "HDD":
-                            table = 'class_ir_attachmentsepp_rel'
+                            table = 'class_ir_attachmentshdd_rel'
                             field_id = 'attachment_ids_hdd'
                         if sub_folder_name == "EPP":
-                            table = 'class_ir_attachmentshdd_rel'
+                            table = 'class_ir_attachmentsepp_rel'  
                             field_id = 'attachment_ids_epp'
                         self._cr.execute("""
                                                 INSERT INTO {0} (class_id,
